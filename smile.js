708b26d9d4463e58d768bab95a3eda0e0af961b6
Gopal <gopalloharnew@gmail.com>
2022-06-10 17:32:12 +0530

    Coronavirus cursor

--- smile.js
+++ smile.js
@@ -1,24 +1,17 @@
 const eyeBalls = document.querySelectorAll("[data-eye-ball]")
+const cursor = document.querySelector("#cursor")
 
 function eyeAnimate(eyeBall, e) {
   const eyeBallRect = eyeBall.getBoundingClientRect()
   const ballX = eyeBallRect.x + eyeBallRect.width / 2
   const ballY = eyeBallRect.y + eyeBallRect.height / 2
 
-  // let dy = e.y - ballY
-  // let dx = e.x - ballX
-  // let theta = Math.atan2(dy, dx)
-  // theta *= 180 / Math.PI
-  // if (theta < 0) theta = 360 + theta
-
-  // eyeBall.style.transform = `rotate(${theta}deg)`
-
   let dy = e.y - ballY
   let dx = e.x - ballX
   let theta = Math.atan2(dy, dx)
 
-  let translateX = Math.cos(theta) * 40
-  let translateY = Math.sin(theta) * 40
+  let translateX = (Math.cos(theta) * 40).toFixed(2)
+  let translateY = (Math.sin(theta) * 40).toFixed(2)
   if (e.target.closest(".eye")) {
     eyeBall.style.transform = `translate(0%, 0%) scale(1.1)`
   } else {
@@ -28,6 +21,8 @@ function eyeAnimate(eyeBall, e) {
 
 function mouseMove(e) {
   eyeBalls.forEach((eyeBall) => eyeAnimate(eyeBall, e))
+  cursor.style.left = e.x + "px"
+  cursor.style.top = e.y + "px"
 }
 
 document.addEventListener("mousemove", mouseMove)
