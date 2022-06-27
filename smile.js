d2181753f3e3b516ece7800ae51fe5cfec1acce6
Gopal <gopalloharnew@gmail.com>
2022-06-27 21:42:44 +0530

    smile v1.0.0

--- smile.js
+++ smile.js
@@ -1,28 +1,50 @@
 const eyeBalls = document.querySelectorAll("[data-eye-ball]")
 const cursor = document.querySelector("#cursor")
+const smile = document.querySelector(".smile-circle")
+const smileRect = smile.getBoundingClientRect()
 
-function eyeAnimate(eyeBall, e) {
-  const eyeBallRect = eyeBall.getBoundingClientRect()
-  const ballX = eyeBallRect.x + eyeBallRect.width / 2
-  const ballY = eyeBallRect.y + eyeBallRect.height / 2
+function setEyeball(x, y, eyeball, eyeballRect, target) {
+  cursor.style.left = x + "px"
+  cursor.style.top = y + "px"
+  let mouseP = y - (eyeballRect.y + eyeballRect.height / 2)
+  let mouseB = x - (eyeballRect.x + eyeballRect.width / 2)
+  let mouseH = Math.hypot(mouseP, mouseB)
 
-  let dy = e.y - ballY
-  let dx = e.x - ballX
-  let theta = Math.atan2(dy, dx)
-
-  let translateX = (Math.cos(theta) * 40).toFixed(2)
-  let translateY = (Math.sin(theta) * 40).toFixed(2)
-  if (e.target.closest(".eye")) {
-    eyeBall.style.transform = `translate(0%, 0%) scale(1.1)`
-  } else {
-    eyeBall.style.transform = `translate(${translateX}%, ${translateY}%)`
+  let eyeBallH = 3
+  let eyeBallSize = 1
+  if (target.closest(".eye")) {
+    eyeBallH = 0
+    eyeBallSize = 1.25
   }
+  let addSad =
+    x > smileRect.x &&
+    y > smileRect.y &&
+    x < smileRect.x + smileRect.width &&
+    y < smileRect.y + smileRect.height
+  eyeball.classList.toggle("red-eye", target.closest(".eye"))
+  smile.classList.toggle("sad", addSad)
+  let eyeBallP = (mouseP / mouseH) * eyeBallH
+  let eyeBallB = (mouseB / mouseH) * eyeBallH
+  eyeball.style.transform = `translate(${eyeBallB}vmin, ${eyeBallP}vmin) scale(${eyeBallSize})`
 }
 
-function mouseMove(e) {
-  eyeBalls.forEach((eyeBall) => eyeAnimate(eyeBall, e))
-  cursor.style.left = e.x + "px"
-  cursor.style.top = e.y + "px"
+function track(eyeball) {
+  const eyeballRect = eyeball.getBoundingClientRect()
+  document.addEventListener("mousemove", (e) => {
+    setEyeball(e.x, e.y, eyeball, eyeballRect, e.target)
+  })
+
+  document.addEventListener(
+    "touchmove",
+    (e) => {
+      let a = e.changedTouches[0]
+      console.log(e.changedTouches)
+      e.preventDefault()
+      setEyeball(a.pageX, a.pageY - 100, eyeball, eyeballRect, a.target)
+    },
+    { passive: false }
+  )
 }
 
-document.addEventListener("mousemove", mouseMove)
+track(eyeBalls[0])
+track(eyeBalls[1])
